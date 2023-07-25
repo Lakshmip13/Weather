f73290a00ddddca6bfaf4186bf4d7a09ae950b65
Lakshmip13 <134908829+Lakshmip13@users.noreply.github.com>
2023-07-25 18:29:37 +0530

    message

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+    return ( 
+       <nav>
+         <div id="logo">
+            <h1>Weather ⛅ </h1>
+        </div>
+       </nav>
+       
+     );
+}
+ 
+export default Navbar;
